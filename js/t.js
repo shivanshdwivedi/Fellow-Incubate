38f42779c255ce968e0f336b796f177a30779075
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-10-14 16:55:20 +0530

    commit

---
+++ js/t.js
@@ -0,0 +1,8 @@
+<script language="JavaScript" type="text/javascript">
+$(document).ready(function(){
+  $('.carouseles').carouseles({
+    interval: 3000
+  })
+});    
+</script>  
+ <script src="js/jquery-3.2.1.min.js"></script>
